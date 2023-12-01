714114f4d35140da69b43e0bc243bf9ec9db86b1
priya <kqvm471@astrazeneca.net>
2023-12-01 14:23:18 +0530

    autopush

---
+++ 02-EscapeSeqChar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "welcome to shell scripting"
+echo " this ismy first class"
+echo " Aws Devops is very bright future"
+echo  -e "line1\nline2"
+echo  -e "line3\nline4"
+echo  -e "line5\tline6"
