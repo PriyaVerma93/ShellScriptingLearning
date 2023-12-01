cf623c1242ee9bf462d91b3969f82920d49eda7a
priya <kqvm471@astrazeneca.net>
2023-12-01 14:39:19 +0530

    autopush

--- 03-colors.sh
+++ 03-colors.sh
@@ -6,4 +6,5 @@
 echo -e "\e[32m I am priniting red color \e[0m"
 echo -e "\e[33m I am priniting red color \e[0m"
 echo -e "\e[34m I am priniting red color \e[0m"
-echo -e "\e[35m I am priniting red color \e[0m"
+
+echo -e "\e[43;32m I am priniting both color" 
