7131f8eb2cd71ad09d38d18e115a033426d95b9e
priya <kqvm471@astrazeneca.net>
2023-12-05 15:14:32 +0530

    autopush

--- 05-dynamicvar.sh
+++ 05-dynamicvar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DATE = $(date + %F)
+DATE=$(date + %F)
 echo "Today date is $DATE"
 
-No_Of_Sessions = $(who|wc -l)
+No_Of_Sessions=$(who|wc -l)
 echo "No of users login: $(No_Of_Sessions)"
