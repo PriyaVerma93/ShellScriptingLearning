91cbd413358f2f049b98a0068b3a595adfa36322
priya <kqvm471@astrazeneca.net>
2023-12-06 16:08:18 +0530

    autopush

--- 11-ifelse.sh
+++ 11-ifelse.sh
@@ -7,7 +7,10 @@ ACTION=$1
 if [ "$ACTION" == "start" ] ; then
   echo "start shipping service"
   exit 0
-else
-  echo " valid option sis start only"
+elif [ "$ACTION" == "stop" ] ; then
+  echo "stopping shipping service"
   exit 1
+else
+  echo " valid option is start and stop only"
+  exit 2
 fi
