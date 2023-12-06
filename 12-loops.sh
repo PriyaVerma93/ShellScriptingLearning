94c6a8d83907212f544dd33bfed279a98c248a02
priya <kqvm471@astrazeneca.net>
2023-12-06 16:35:32 +0530

    autopush

---
+++ 12-loops.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#loops- when you want to run an action certain no of times, we use loops
+#For Loop- when you know something ha sto executed certain no of times, we use for loop
+#While loops- when you donot know something to be executed n no of times, we use while loop
+
+
+for i in 10 20 30 40 50 60 ; do 
+  echo "values from loops are $i"
+done
