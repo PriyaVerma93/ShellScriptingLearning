b0003d0bc502033815038fb4605f282da35df593
priya <kqvm471@astrazeneca.net>
2023-12-06 18:36:20 +0530

    autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,4 +8,9 @@ fi
 
 echo -e ****************"\e[32m confguring frontend \e[0m"******
 echo -e "\e[33mInstalling Nginx\e[0m"
-yum install nginx -y > /tmp/frontend.log
+yum install nginx -y &>> /tmp/frontend.log
+if  [ $? = 0 ] ; then
+   echo -e "\e[34mSuccess\e[0m"
+else
+   echo -e "\e[35mFailuree[0m"
+fi 
