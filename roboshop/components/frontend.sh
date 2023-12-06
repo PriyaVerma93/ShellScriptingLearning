f241d4c51e18bfd4cd6175e72fa08ff92370edfe
priya <kqvm471@astrazeneca.net>
2023-12-06 18:25:30 +0530

    autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-
-
+uid=$(id -u)
+if  [ $uid -ne 0 ] ; then
+   echo -e "\e[32mThis script is expected to executed by root user\e[0m"
+   exit 20
+fi 
 
 echo -e ****************"\e[32m confguring frontend \e[0m"******
 echo -e "\e[33mInstalling Nginx\e[0m"
--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash components/$1.sh
 
-if  [ $? -ne 0 ] ; then
-   echo -e "\e[32m example usgae : bash wrapper.sh componentname \e[0m"
-   exit 20
-fi 
+#if  [ $? -ne 0 ] ; then
+   #echo -e "\e[32m example usgae : bash wrapper.sh componentname \e[0m"
+   #exit 20
+#fi 
