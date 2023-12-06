e57229f0b1476d30b9db43132af47a35257a4803
priya <kqvm471@astrazeneca.net>
2023-12-06 14:39:26 +0530

    autopush

---
+++ 10-redirections.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#two types of redirectors
+1)input redirection (<)
+2)output redirection(>)
+
+$ACTION=$1
+case $ACTION in 
+   start)
+       echo -e "\e[32m  starting shipping service \e[0m"
+       exit 0
+       ;;
+    stop)
+       echo -e "\e[30m  stopping shipping service \e[0m"
+       exit 1
+       ;;
+    restart)
+       echo -e "\e[30m  restarting shipping service \e[0m"
+       exit 2
+       ;;
+     *)
+     echo -e "\e[32m valid options are start stop or Restart\e[0m]"
+
+
+
+
+esac
