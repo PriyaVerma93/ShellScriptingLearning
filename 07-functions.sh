b77f46ebb941e97d5da49e92ad64425fa5ff8621
priya <kqvm471@astrazeneca.net>
2023-12-06 12:57:57 +0530

    autopush

---
+++ 07-functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+b56() {
+    echo "This training is about Devops"
+}
+
+stat() {
+    echo "No of session opened $(who|wc -l)"
+    echo "hai"
+    echo "Todays date is $(date +%F)
+}
