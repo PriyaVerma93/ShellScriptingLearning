dafe381a97b00a39b7254ed2dfee7505684718e5
priya <kqvm471@astrazeneca.net>
2023-12-06 17:56:32 +0530

    autopush

--- roboshop/components/wrapper.sh
+++ roboshop/components/wrapper.sh
@@ -2,6 +2,6 @@
 bash components/$1.sh
 
 if  [ $? -ne 0 ] ; then
-   echo "\e[32m example usgae : \e[0m bash wrapper.sh componentname"
+   echo "\e[32m example usgae : bash wrapper.sh componentname \e[0m"
    exit 20
 fi 
