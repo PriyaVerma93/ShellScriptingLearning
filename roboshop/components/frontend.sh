9c74507b7f731bf58ca843e2fe49fc955f1c8384
priya <kqvm471@astrazeneca.net>
2023-12-07 14:58:28 +0530

    autopush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,30 +7,26 @@ if  [ $uid -ne 0 ] ; then
    exit 20
 fi 
 
-echo -e ****************"\e[32m confguring frontend \e[0m"******
-echo -n "***Installing Nginx***"
-yum install nginx -y &>> /tmp/frontend.log
-if  [ $? = 0 ] ; then
+stat() {if  [ $1 = 0 ] ; then
    echo -e "\e[34mSuccess\e[0m"
 else
    echo -e "\e[35mFailuree[0m"
-fi 
+fi
+}
+
+echo -e ****************"\e[32m confguring frontend \e[0m"******
+echo -n "***Installing Nginx***"
+yum install nginx -y &>> /tmp/frontend.log
+stat $?
+
 echo -n "Downloading the Component $1 :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if  [ $? = 0 ] ; then
-   echo -e "\e[34m component downloaded successfully \e[0m"
-else
-   echo -e "\e[35m Downloading Failure [0m"
-fi
+stat $?
 
 echo -n "cleanup of $1 component"
 cd /usr/share/nginx/html
 rm -rf * &>> /tmp/frontend.log
-if  [ $? = 0 ] ; then
-   echo -e "\e[34mSuccess\e[0m"
-else
-   echo -e "\e[35mFailuree[0m"
-fi
+stat $?
 
 echo -n "Extracting the $1 component"
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
@@ -38,20 +34,11 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-if  [ $? = 0 ] ; then
-   echo -e "\e[34mSuccess\e[0m"
-else
-   echo -e "\e[35mFailuree[0m"
-fi
+stat $?
 
 echo -n "Restarting $1 component"
  systemctl enable nginx &>> /tmp/frontend.log
  systemctl daemon-reload &>> /tmp/frontend.log
  systemctl restart nginx &>> /tmp/frontend.log
- if  [ $? = 0 ] ; then
-   echo -e "\e[34mSuccess\e[0m"
-else
-   echo -e "\e[35mFailuree[0m"
-fi
-
+stat $?
 
